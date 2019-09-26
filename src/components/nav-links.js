77095bf98d73283176faca0a56b3841f878b12c2
Feng <afeng@urban.org>
2019-09-26 11:27:35 -0400

    Write NavLinks component correctly

--- src/components/nav-links.js
+++ src/components/nav-links.js
@@ -3,16 +3,11 @@ import React from "react"
 import navLinksStyles from "./nav-links.module.css"
 
 const NavLinks = () => (
-  <navLinks
-    style={{
-    }}
-  >
     <div className={navLinksStyles.navLinkContainer}>
         <Link to="/" className={navLinksStyles.navLink}>Overview</Link>
         <Link to="/issue-areas/" className={navLinksStyles.navLink}>Issue Areas</Link>
         <Link to="/tax-policies/" className={navLinksStyles.navLink}>Tax Policies</Link>
     </div>
-  </navLinks>
 )
 
 export default NavLinks
