b48616f30a56a1c5a858412c68405cd0519efd7a
Feng <afeng@urban.org>
2019-10-01 14:49:34 -0400

    Add overlay behind modal and adjust modal styles

--- src/components/modal.js
+++ src/components/modal.js
@@ -2,17 +2,23 @@ import React from "react"
 import modalStyles from "./modal.module.css"
 
 function Modal(props) {
+    if(!props.isOpen) {
+        return null;
+    }
     return (
-        <div className={modalStyles.modal + " " + (props.isOpen ? "" : modalStyles.closed)}>
-            This is the modal <br />
-            {props.candidate} <br />
-            {props.view}
-            <button
-                className={modalStyles.closeModalBtn}
-                onClick={() => props.onClick()}
-            >
-                Close
-            </button>
+        <div>
+            <div className={modalStyles.modalBackdrop}></div>
+            <div className={modalStyles.modal + " " + (props.isOpen ? null : modalStyles.closed)}>
+                <h3>This is the modal</h3>
+                {props.candidate} <br />
+                {props.view}
+                <button
+                    className={modalStyles.closeModalBtn}
+                    onClick={() => props.onClick()}
+                >
+                    Close
+                </button>
+            </div>
         </div>
     );
 }
