f4729c60d4a87ef1643e7ce4c9578b3085b866f1
Feng <afeng@urban.org>
2019-10-07 12:42:08 -0400

    Build out function for creating Cartesian product of two arrays, show one card per candidate-issue

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,21 +1,45 @@
 import React from "react"
 import cardStyles from "./cards.module.css"
 
+const cartProd = (arr1, arr2) =>
+    arr1.flatMap(x => arr2.map(y => [x, y]));
+
 function Card(props) {
+    // const view = props.view;
     const candidate = props.candidate;
+    // let cardText;
+
+    // if(view === "issue areas") cardText = props.issue;
+
     return (
-        <div className={cardStyles.card} onClick={() => props.onClick(candidate.name)}>{candidate.name}<br />{props.view}</div>
+        <div
+            className={cardStyles.card}
+            onClick={() => props.onClick(candidate)}
+        >
+            {candidate} <br />
+            {props.view}
+        </div>
     )
 }
 
 function Cards(props) {
-    const candidateCards = props.candidates.filter((candidate) => candidate.selected).map((candidate) =>
-        <Card
-            key={candidate.name}
-            candidate={candidate}
-            view={props.view}
-            onClick={props.onClick}
-        />
+    const view = props.view;
+    const selectedCandidates = props.candidates.filter((candidate) => candidate.selected).map((candidate) => candidate.name);
+    let allCards = selectedCandidates;
+
+    if(view === "issue areas") {
+        const selectedIssues = props.issues.filter((issue) => issue.selected).map((issue) => issue.name);
+        allCards = cartProd(selectedCandidates, selectedIssues);
+    }
+
+    console.log(allCards);
+    const candidateCards = allCards.map((candidate) =>
+            <Card
+                key={candidate}
+                candidate={candidate}
+                view={view}
+                onClick={props.onClick}
+            />
     );
 
     return (
