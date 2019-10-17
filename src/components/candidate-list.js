ebcbbc7e0d9ae1a4c61ffd624eb7da7d988551bb
Feng <afeng@urban.org>
2019-10-17 11:34:23 -0400

    Start styling candidate menu list

--- src/components/candidate-list.js
+++ src/components/candidate-list.js
@@ -6,7 +6,7 @@ function CandidateList(props) {
     const allCandidateListItems = candidates.map((candidate, idx) =>
         <li key={candidate.id} onDragOver={() => props.onDragOver(idx)}>
             <button
-                className={candidateListStyles.menuButton + " " + (candidate.selected ? candidateListStyles.selected : null)}
+                className={candidateListStyles.btn + " " + candidateListStyles.menuButton + " " + (candidate.selected ? candidateListStyles.selected : null)}
                 onClick={() => props.onClick(candidate.id)}
                 draggable
                 onDragStart={(e) => props.onDragStart(e, idx)}
@@ -19,11 +19,31 @@ function CandidateList(props) {
 
     return (
         <>
-            <div>
-                <h4>Choose candidates</h4>
-                <button onClick={() => props.onSelectAllClick("candidates")}>Select all</button>
-                /
-                <button onClick={() => props.onClearSelectionClick("candidates")}>Clear selection</button>
+            <div style={{maxWidth: 300}}>
+                <h4
+                    style={{
+                        fontSize: 24,
+                        fontWeight: `bold`,
+                        marginBottom: `1rem`
+                     }}
+                >
+                    Choose candidates
+                </h4>
+                <div style={{marginBottom: 20}}>
+                    <button
+                        className={candidateListStyles.btn + " " + candidateListStyles.selectAllBtn}
+                        onClick={() => props.onSelectAllClick("candidates")}
+                    >
+                        Select all
+                    </button>
+                    <span style={{padding: 10, fontWeight: `bold`}}>/</span>
+                    <button
+                        className={candidateListStyles.btn + " " + candidateListStyles.selectAllBtn}
+                        onClick={() => props.onClearSelectionClick("candidates")}
+                    >
+                        Clear selection
+                    </button>
+                </div>
             </div>
             <ul className={candidateListStyles.menuList}>{allCandidateListItems}</ul>
         </>
