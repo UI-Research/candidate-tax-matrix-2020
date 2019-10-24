486bb549c52c1ec3bb57f6ed2f623c11fe5aa546
Feng <afeng@urban.org>
2019-10-24 17:08:23 -0400

    Updated data generation script based on new spreadsheet-based text format

--- src/data/makeData.py
+++ src/data/makeData.py
@@ -24,58 +24,37 @@ Ouputs:
     
 @author: afeng
 """
-import numpy as np
 import pandas as pd
 import json
 
+# TODO: update the following lines of code
 candidates = pd.read_csv("candidates.csv")
 issue_areas = pd.read_csv("issue_areas.csv")
 tax_types = pd.read_csv("tax_types.csv")
 
-text_to_cat = pd.read_csv("text_to_category_mapping.csv")
+candidate_text = pd.read_excel("Candidate text.xlsx", sheet_name = "Master", 
+                               usecols = "A:F", 
+                               names = ["Candidate", "Tax1", "Tax2", "Issue1", "Issue2", "Text"])
 
-with open("matrix_text.json") as json_file:
-    text = json.load(json_file)
-
-def makeBulletText(candidate, view, category):
-    bullets = bullets_mapped[(bullets_mapped.candidate == candidate) & 
-                             (bullets_mapped.type == view) &
-                              (bullets_mapped.category == category)]
-    if(len(bullets) > 0):
-        bullet_text = []
-        for bullet_num in bullets.bullet_number:
-            bullet_text.append(text[candidate]["bullet " + str(bullet_num)])
-    else:
-        bullet_text = ["None"]
-        
-    return bullet_text
-
+master_lists = pd.read_excel("Candidate text.xlsx", sheet_name = "Lists (Don't delete!)",
+                             usecols = "A:E",
+                             names = ["Candidate_first", "Candidate_last", "Candidate_party", "Tax_types", "Issue_areas"])
 
 # make master data jsons for candidate, issue areas and tax policies filter lists
 candidates["selected"] = True
 candidates.to_json("candidates.json", orient = "records")
 
+issue_areas = master_lists["Issue_areas"][master_lists["Issue_areas"].notnull()]
+
 issue_areas["selected"] = True
 issue_areas.to_json("issue_areas.json", orient = "records")
 
+tax_types = master_lists["Tax_types"][master_lists["Tax_types"].notnull()]
+
 tax_types["selected"] = True
 tax_types.to_json("tax_types.json", orient = "records")
-
-# make json with candidate bullet points
-bullets_mapped = text_to_cat.merge(issue_areas, 
-                                   how="left", 
-                                   left_on = "category_id", 
-                                   right_on = "id"
-                                   ).merge(tax_types,
-                                   how="left",
-                                   left_on = "category_id",
-                                   right_on = "id")
-
-bullets_mapped["category"] = np.where(bullets_mapped["type"] == "Issue area", bullets_mapped["name_x"], bullets_mapped["name_y"])
-bullets_mapped.drop(["category_id", "id_x", "name_x", "id_y", "name_y"], 
-                    axis = "columns", 
-                    inplace = True)
-
+    
+# make skeleton json
 data = []
 
 for candidate in candidates["last_name"]:
@@ -86,13 +65,41 @@ for candidate in candidates["last_name"]:
     candidate_dict["Issue areas"] = {}
     candidate_dict["Tax types"] = {}
     
-    for issue in issue_areas["name"]:
-        candidate_dict["Issue areas"][issue] = makeBulletText(candidate, "Issue area", issue)
+    for issue in issue_areas:
+        candidate_dict["Issue areas"][issue] = "None"
     
-    for tp in tax_types["name"]:
-        candidate_dict["Tax types"][tp] = makeBulletText(candidate, "Tax type", tp)
+    for tp in tax_types:
+        candidate_dict["Tax types"][tp] = "None"
 
     data.append(candidate_dict)
+
+# group together all text items that apply to the same issue area or tax type
+# per candidate and put them in a list
+ia1 = candidate_text[["Candidate", "Issue1", "Text"]].rename(columns={"Issue1": "Issue"})
+ia2 = candidate_text[["Candidate", "Issue2", "Text"]].rename(columns={"Issue2": "Issue"})
+ia2.dropna(subset=["Issue"], inplace=True)
+candidate_ia = pd.concat([ia1, ia2])
+candidate_ia_grouped = candidate_ia.groupby(["Candidate", "Issue"])["Text"].apply(list).reset_index()
+
+tp1 = candidate_text[["Candidate", "Tax1", "Text"]].rename(columns={"Tax1": "Tax type"})
+tp2 = candidate_text[["Candidate", "Tax2", "Text"]].rename(columns={"Tax2": "Tax type"})
+tp2.dropna(subset=["Tax type"], inplace=True)
+candidate_tp = pd.concat([tp1, tp2])
+candidate_tp_grouped = candidate_tp.groupby(["Candidate", "Tax type"])["Text"].apply(list).reset_index()
+
+# populate json where applicable with text from spreadsheet
+for row in data:
+    candidate = row["Name"]
+    for issue in row["Issue areas"]:
+        text = candidate_ia_grouped.loc[(candidate_ia_grouped.Candidate == candidate) & (candidate_ia_grouped.Issue == issue), "Text"]
+        if(len(text) > 0):
+            row["Issue areas"][issue] = text.values[0]
+
+    for tp in row["Tax types"]:
+        text = candidate_tp_grouped.loc[(candidate_tp_grouped.Candidate == candidate) & (candidate_tp_grouped["Tax type"] == tp), "Text"]
+        if(len(text) > 0):
+            row["Tax types"][tp] = text.values[0]
+            
     
 with open("data.json", "w") as outfile:
     json.dump(data, outfile)
