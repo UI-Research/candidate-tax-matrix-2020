adebd208980bc714cce2a6a454f71b742c1d7ec9
Feng <afeng@urban.org>
2019-10-30 14:01:46 -0400

    Populate top part of modal through separator line

--- src/components/modal.js
+++ src/components/modal.js
@@ -2,6 +2,35 @@ import React from "react"
 import cardData from "../data/data.json"
 import modalStyles from "./modal.module.css"
 
+function ModalContent(props) {
+    let contentGroup = <h4>Overview</h4>
+    if(props.view === "Overview") {
+    }
+    else {
+        const data = cardData[props.candidateLastName][props.view];
+        let topics = Object.keys(data).sort();
+        let selectedTopicPos = topics.indexOf(props.topic);
+        topics.splice(selectedTopicPos, 1);
+        topics.unshift(props.topic);  // put selected topic first with all other topics in alphabetical order afterwards
+        console.log(cardData[props.candidateLastName]);
+
+        const contentBullets = cardData[props.candidateLastName][props.view][props.topic].map((bullet, index) =>
+            <li key={index}>{bullet}</li>
+        );
+
+        contentGroup = <div>
+            <h4 className={modalStyles.topicSubhead + " " + (props.party === "Democrat" ? modalStyles.democrat : modalStyles.republican)}>Proposal on {props.topic.toLowerCase()}</h4>
+            <ul className={modalStyles.contentList}>
+                {contentBullets}
+            </ul>
+            <div className={modalStyles.separator}><span class={modalStyles.separatorLine}></span>Other proposals by {props.view === "Issue areas" ? "issue area" : "type of tax"}<span className={modalStyles.separatorLine}></span></div>
+        </div>;
+    }
+    return (
+        <div className="contentContainer">{contentGroup}</div>
+    )
+}
+
 function Modal(props) {
     if(!props.isOpen) {
         return null;
@@ -37,6 +66,12 @@ function Modal(props) {
                 >
                     ×
                 </button>
+                <ModalContent
+                    candidateLastName={candidateLastName}
+                    view={view}
+                    topic={topic}
+                    party={party}
+                />
             </div>
         </div>
     );
