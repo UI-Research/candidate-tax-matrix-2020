45f84758581199d7e35579fa6572e1d8cbe0bf99
Feng <afeng@urban.org>
2019-10-30 14:24:16 -0400

    Adjust some wording and finish populating modals for issue areas and tax types

--- src/components/cards.js
+++ src/components/cards.js
@@ -48,7 +48,7 @@ function Card(props) {
                 <div className={cardStyles.partyLogo + " " + (party === "Democrat" ? cardStyles.democrat : cardStyles.republican)}>{party === "Democrat" ? "D" : "R"}</div>
                 <h3 className={cardStyles.candidateName + " " + (party === "Democrat" ? cardStyles.democrat : cardStyles.republican)}>{candidateFirstName + " " + candidateLastName}</h3>
             </div>
-            <h4 className={cardStyles.sectionTitle + " " + (party === "Democrat" ? cardStyles.democrat : cardStyles.republican)}>Position</h4>
+            <h4 className={cardStyles.sectionTitle + " " + (party === "Democrat" ? cardStyles.democrat : cardStyles.republican)}>Proposal</h4>
             <ul className={cardStyles.contentList}>
                 {cardBullets}
             </ul>
--- src/components/modal.js
+++ src/components/modal.js
@@ -2,32 +2,51 @@ import React from "react"
 import cardData from "../data/data.json"
 import modalStyles from "./modal.module.css"
 
-function ModalContent(props) {
-    let contentGroup = <h4>Overview</h4>
-    if(props.view === "Overview") {
-    }
-    else {
-        const data = cardData[props.candidateLastName][props.view];
-        let topics = Object.keys(data).sort();
-        let selectedTopicPos = topics.indexOf(props.topic);
-        topics.splice(selectedTopicPos, 1);
-        topics.unshift(props.topic);  // put selected topic first with all other topics in alphabetical order afterwards
-        console.log(cardData[props.candidateLastName]);
+function ContentDiv(props) {
+    const contentBullets = props.data.map((bullet, index) =>
+        <li key={index}>{bullet}</li>
+    );
 
-        const contentBullets = cardData[props.candidateLastName][props.view][props.topic].map((bullet, index) =>
-            <li key={index}>{bullet}</li>
-        );
-
-        contentGroup = <div>
+    return (
+        <div>
             <h4 className={modalStyles.topicSubhead + " " + (props.party === "Democrat" ? modalStyles.democrat : modalStyles.republican)}>Proposal on {props.topic.toLowerCase()}</h4>
             <ul className={modalStyles.contentList}>
                 {contentBullets}
             </ul>
-            <div className={modalStyles.separator}><span class={modalStyles.separatorLine}></span>Other proposals by {props.view === "Issue areas" ? "issue area" : "type of tax"}<span className={modalStyles.separatorLine}></span></div>
-        </div>;
+        </div>
+    )
+}
+
+function ModalContent(props) {
+    let remainingTopics = <h4>Overview</h4>
+    let selectedTopicData;
+    if(props.view === "Overview") {
+    }
+    else {
+        const data = cardData[props.candidateLastName][props.view];
+        selectedTopicData = data[props.topic];
+
+        let topics = Object.keys(data).sort();
+        let selectedTopicPos = topics.indexOf(props.topic);
+        topics.splice(selectedTopicPos, 1);
+        remainingTopics = topics.map((topic) =>
+            <ContentDiv
+                party={props.party}
+                topic={topic}
+                data={data[topic]}
+            />
+        );
     }
     return (
-        <div className="contentContainer">{contentGroup}</div>
+        <div className="contentContainer">
+            <ContentDiv
+                party={props.party}
+                topic={props.topic}
+                data={selectedTopicData}
+            />
+            <div className={modalStyles.separator}><span className={modalStyles.separatorLine}></span>Other proposals by {props.view === "Issue areas" ? "issue area" : "type of tax"}<span className={modalStyles.separatorLine}></span></div>
+            {remainingTopics}
+        </div>
     )
 }
 
