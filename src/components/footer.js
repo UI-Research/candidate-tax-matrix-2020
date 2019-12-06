d077129a231373eb44108fc4e4fde53adf6c1582
Feng <afeng@urban.org>
2019-12-05 21:00:40 -0500

    Add in Methodology pdf

--- src/components/footer.js
+++ src/components/footer.js
@@ -17,11 +17,13 @@ const Footer = () => (
                 padding: `1.45rem 1.0875rem`,
             }}
         >
-            <h4 className={footerStyles.footerHeader}>About</h4>
+            <h4 className={footerStyles.footerHeader}>About the data</h4>
+            <p className={footerStyles.footerText}>View and download the <a href="../../Methodology.pdf" target="_blank" rel="noopener noreferrer">technical appendix</a>.</p>
+
+            <h4 className={footerStyles.footerHeader}>Project Credits</h4>
             <p className={footerStyles.footerText}>
                 This feature was funded by <a href="https://www.arnoldventures.org/" target="_blank" rel="noopener noreferrer">Arnold Ventures</a>. We are grateful to them and to all our funders, who make it possible for the Tax Policy Center to advance its mission. The views expressed are those of the authors and should not be attributed to the Urban Institute, the Brookings Institution, their trustees, or their funders. Funders do not determine research findings or the insights and recommendations of Tax Policy Center experts. More information on the Tax Policy Center’s funding principles is available <a href="http://www.taxpolicycenter.org/support/funding-principles" target="_blank" rel="noopener noreferrer">here</a>. Read Urban’s terms of service <a href="https://www.urban.org/terms-service" target="_blank" rel="noopener noreferrer">here</a>.
             </p>
-            <h4 className={footerStyles.footerHeader}>Project Credits</h4>
             <h5 className={footerStyles.job}>Research</h5>
             <p className={footerStyles.footerText}><a href="https://www.urban.org/author/janet-holtzblatt" target="_blank" rel="noopener noreferrer">Janet Holtzblatt</a> and <a href="https://www.urban.org/author/nikhita-airi" target="_blank" rel="noopener noreferrer">Nikhita Airi</a></p>
             <h5 className={footerStyles.job}>Design</h5>
