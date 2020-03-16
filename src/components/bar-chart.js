a3c723c6a535994dbacd24d85e314435d799a184
Feng <afeng@urban.org>
2020-03-16 13:51:52 -0400

    Add in tick marks on the y-axis

--- src/components/bar-chart.js
+++ src/components/bar-chart.js
@@ -2,17 +2,17 @@ import React from "react"
 import analysisData from "../data/analysis_data.json"
 import { scaleLinear, scaleBand } from "d3-scale"
 // import chartStyles from "./bar-chart.module.css"
-const chartWidth = 150,
+const chartWidth = 300,
       chartHeight = 150,
-      padding = {top: 20, bottom: 0, left: 0, right: 0};
+      padding = {top: 20, bottom: 5, left: 35, right: 0};
 
 const xScale = scaleBand()
     .domain(["Lowest", "Second", "Middle", "Fourth", "Highest"])
     .range([padding.left + 1, chartWidth])
-    .padding(0);
+    .padding(0.2);
 
 const yScale = scaleLinear()
-    .domain([-15, 0])
+    .domain([-12, 0])
     .range([chartHeight - padding.bottom, padding.top]);
 
 const Axis = (direction) => {
@@ -22,7 +22,7 @@ const Axis = (direction) => {
                 <text x={chartWidth / 2} y={padding.top - 8} fill="#000" style={{textAnchor:`middle`}}>Quintile</text>
                 <path
                     d={[
-                        "M", padding.left + 1, padding.top + 1,
+                        "M", padding.left + 1, padding.top,
                         "H", chartWidth
                         ].join(" ")}
                     stroke="#000"
@@ -31,27 +31,23 @@ const Axis = (direction) => {
         )
     }
     else if(direction === "y") {
-        const ticks = yScale.ticks().map(value => ({
+        const ticks = yScale.ticks(3).map(value => ({
                     value,
                     yOffset: yScale(value)
             }));
-// console.log(ticks);
+
         return (
             <g className="axis y">
-                <path
+             {/*   <path
                     d={[
-                        "M", padding.left + 1, padding.top + 1,
+                        "M", padding.left + 1, padding.top,
                         "V", chartHeight - padding.bottom
                     ].join(" ")}
                     stroke="#000"
                 />
-          {/*      {ticks.map(({value, yOffset}) => (
+            */}
+                {ticks.map(({value, yOffset}) => (
                     <g key={value} transform={`translate(0, ${yOffset})`} className="tick">
-                        <line
-                            x1="14"
-                            x2={padding.left}
-                            stroke="#000"
-                        />
                         <line
                             x1={padding.left}
                             x2={chartWidth}
@@ -59,15 +55,17 @@ const Axis = (direction) => {
                         />
                         <text
                             key={value}
+                            x={padding.left - 5}
+                            y={0.5}
+                            dy={`0.32em`}
                             style={{
-                                fontSize: "10px",
-                                textAnchor: "start",
-                                transform: "translateY(4px)"
+                                fontSize: "12px",
+                                textAnchor: "end",
                             }}>
-                            { value }
+                            { value }%
                         </text>
                     </g>
-                ))} */}
+                ))}
             </g>
         )
     }
@@ -93,10 +91,10 @@ function BarChart(props) {
     const yAxis = Axis("y");
 
     return (
-        <svg width={chartWidth} height={chartHeight} viewBox="0 0 150 150" style={{marginBottom:`1rem`}}>
-            {bars}
-            {xAxis}
+        <svg width={chartWidth} height={chartHeight} viewBox="0 0 300 150" style={{marginBottom:`1rem`}}>
             {yAxis}
+            {xAxis}
+            {bars}
         </svg>
     )
 }
